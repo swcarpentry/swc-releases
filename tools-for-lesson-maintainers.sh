bf544760c91ac63b438dc663b2cb6db083dd5ffe
Rémi Emonet <remi.emonet@reverse--com.heeere>
2016-05-30 21:49:10 +0200

    Updating tool for "lesson maintainers"

--- tools-for-lesson-maintainers.sh
+++ tools-for-lesson-maintainers.sh
@@ -1,8 +1,9 @@
 
 # Meant to be source'd
 
+# same as in "make-or-update-release.sh" but for write access
 resolve-lesson() {
-    printf %s "https://github.com/swcarpentry/$1"
+    printf %s "git@github.com:swcarpentry/$1.git"
 }
 
 # this one may need to be updated once we use the new build system
@@ -14,13 +15,17 @@ clone-branch-build-commit() {
     as=$1
     vers=$2
     repo=$(resolve-lesson $as)
-    git clone $repo ,,$as --depth=1 \
-        && cd ,,$as \
-        && git checkout -b $vers \
-        && make clean preview \
-        && git add *.html \
-        && git commit -m "Rebuilt HTML files for release $as" \
-        && cd -
+    git clone $repo ,,$as --depth=1 && cd ,,$as && {
+        git checkout -b $vers
+        make clean preview
+        git add *.html
+        git commit -m "Rebuilt HTML files for release $as"
+        git log
+        echo Will git push unless you Ctrl+C
+        read DUMMY
+        git push --set-upstream origin $vers
+        cd -
+    } || cd -
 }
 
 

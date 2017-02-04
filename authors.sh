3c382564672bbf7ce9465388983bc15e54599aa0
Rémi Emonet <remi.emonet@reverse--com.heeere>
2017-02-04 17:54:36 +0100

    Tools and cleaned mailmap, for AUTHORS

--- authors.sh
+++ authors.sh
@@ -2,25 +2,18 @@
 
 set -e
 
-inifile=$1
-shift
-
-test -f $inifile
-echo "Found ini file $inifile"
 
 list-repos() {
     cat $inifile | grep -e '^local_folder = ' | sed 's@[^=]*=@@g'
 }
 
-(for i in $(list-repos) ; do
-     cat $i/.mailmap
- done) \
-    | sed  -e 's@\([^<]*\) <\([^>]*\)> @\1 <\2>\n\1 @g' \
-    | sed  -e 's@\([^<]*\) <\([^>]*\)> @\1 <\2>\n\1 @g' \
-    | sed  -e 's@\([^<]*\) <\([^>]*\)> @\1 <\2>\n\1 @g' | sort | uniq > ,,all-mailmap
-echo "Generated ,,all-mailmap"
-
-
+#(for i in $(list-repos) ; do
+#     cat $i/.mailmap
+# done) \
+#    | sed  -e 's@\([^<]*\) <\([^>]*\)> @\1 <\2>\n\1 @g' \
+#    | sed  -e 's@\([^<]*\) <\([^>]*\)> @\1 <\2>\n\1 @g' \
+#    | sed  -e 's@\([^<]*\) <\([^>]*\)> @\1 <\2>\n\1 @g' | sort | uniq > ,,all-mailmap
+#echo "Generated ,,all-mailmap"
 
 fix-multi-email-in-one-line() {
     local i
@@ -28,10 +21,9 @@ fix-multi-email-in-one-line() {
         sed -e 's@\([^<]*\) <\([^>]*\)> @\1 <\2>\n\1 @g' -i $1
     done
 }
+#fix-multi-email-in-one-line ,,all-mailmap
 
-fix-multi-email-in-one-line ,,all-mailmap
-
-process-repo() {
+process-repo-old() {
     local i=$1
     cd $i
     echo "////////// $i //////////"
@@ -71,20 +63,96 @@ check-pull-requests() {
     done | xargs firefox
 }
 
+check-all-mailmap() {
+    echo "## Looking for duplicates..."
+    cat all-mailmap | sed 's@.*<@<@g'| sort | uniq -c | grep -v '^ *1'   ||true
+    echo "## Looking for single-word name..."
+    cat all-mailmap | grep '^[^ ]* <'    ||true
+}
+
+obfuscate() {
+    # used for commiting cheaply obfuscated emails
+    cp all-mailmap ,,amm
+    cat ,,amm | tr '[a-m][n-z][A-M][N-Z]' '[n-z][a-m][N-Z][A-M]' > all-mailmap
+}
+
+process-repo() {
+    local i
+    for i in $(list-repos); do
+        echo "////////// $i //////////"
+        cd $i
+        # the ones that actually need something in the mail map (they don't match what we have in the shared one)
+        awk 'NR==FNR{a[$0];next}!($0 in a)' ../all-mailmap <(git log --format="%an <%ae>"|sort |uniq) > ../,,diffs
+
+        rm -f ../,,.mailmap
+        cat ../,,diffs | while read line ; do
+            local n="<${line#*<}"
+            repl=$(cat ../all-mailmap | grep -e "$n"'$' | tail -1)
+            if test "$repl" '!=' "" ; then
+                #echo "Recycling: $repl"
+                echo "$repl" >> ../,,.mailmap
+            else
+                echo "From commit (TODO manual integration needed): $line"
+                echo "$line # TODO" >> ../,,.mailmap
+            fi
+        done
+        cat ../,,.mailmap | sort | uniq > .mailmap
+        # Now we have the proper mailmap, go on with AUTHORS
+        # show it first
+        diff <(cat AUTHORS |sort|uniq) <(git log --format="%aN" |sort|uniq) |colordiff
+        # add what is missing (remove nothing)
+        awk 'NR==FNR{a[$0];next}!($0 in a)' AUTHORS <(git log --format="%aN"|sort |uniq) > ../,,diffs
+        cat ../,,diffs | sort | uniq >> AUTHORS
+        # remove what should be
+        awk 'NR==FNR{a[$0];next}!($0 in a)' <(git log --format="%aN" |sort|uniq) <(cat AUTHORS |sort|uniq) > ../,,diffs
+        cat ../,,diffs | while read line ; do
+            echo RM:$line
+            cp AUTHORS ../,,auth
+            cat ../,,auth | grep -v '^'"$line"'$' > AUTHORS
+        done
+        # WARN + remove single-word names
+        cat AUTHORS | grep '^[^ ]*$' | sed 's@^@###### WARN: @g'
+        cp AUTHORS ../,,auth
+        cat ../,,auth | grep -v '^[^ ]*$' > AUTHORS
+        # back
+        cd -
+    done
+}
+
 check-author-diff-summary() {
     local i
     for i in $(list-repos); do
         cd $i
         echo "////////// $i //////////"
         diff <(cat AUTHORS |sort |uniq) <(git log --use-mailmap --format="%aN" |sort |uniq)    ||true
-        # back
         cd -
     done
 }
 
-list-repos
+
+#list-repos
 #process-repo ,,workshop-template
-check-author-diff-summary | colordiff
+#check-author-diff-summary | colordiff
+#check-all-mail-map
+
+inifile=$1
+shift
+
+if test "$inifile" == "obfuscate" ; then
+    obfuscate
+    exit
+fi
+
+test -f $inifile
+echo "Found ini file $inifile"
+
+if grep __OBFUSCATED__ all-mailmap ; then
+    echo "NB: Automatically un-obfuscated all-mailmap"
+    echo "    ... should obfuscate before commit, with: $0 obfscate"
+    obfuscate
+fi
+
+"$@"
 
 #for i in $(list-repos) ; do
 #    process-repo $i
